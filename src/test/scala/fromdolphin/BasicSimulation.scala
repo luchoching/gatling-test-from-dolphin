c112cb890ac7b7510946dddabcd54429ce0310f8
Lu Cho-Ching <choching.work@gmail.com>
2016-03-02 15:18:24 +0800

    Seperate the scenario into composable processes

--- src/test/scala/fromdolphin/BasicSimulation.scala
+++ src/test/scala/fromdolphin/BasicSimulation.scala
@@ -6,6 +6,12 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
+  object Init {
+    val init = exec(http("request_1")
+      .get("/"))
+      .pause(7)
+  }
+
   val httpConf = http
     .baseURL("http://localhost:3000")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
@@ -13,13 +19,9 @@ class BasicSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .wsBaseURL("ws://localhost:8000")
 
-  val headers_10 = Map("Content-Type" -> """application/x-www-form-urlencoded""")
-
-  val scn = scenario("Nav Dolphin")
-    .exec(http("request_1")
-    .get("/"))
-    .pause(7)
+  val scn = scenario("Scenario Name").exec(Init.init)
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
 
