ae7f78efe22f339fe9615defddbe850c485704c0
Lu Cho-Ching <choching.work@gmail.com>
2016-03-02 14:53:47 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+git clone https://github.com/gatling/gatling-sbt-plugin-demo.git && cd gatling-sbt-plugin-demo
+```
---
+++ src/test/scala/fromdolphin/BasicSimulation.scala
@@ -0,0 +1,26 @@
+package fromdolphin
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:3000")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  val headers_10 = Map("Content-Type" -> """application/x-www-form-urlencoded""")
+
+  val scn = scenario("Nav Dolphin")
+    .exec(http("request_1")
+    .get("/"))
+    .pause(7)
+
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+
+}
